988fde01bb91aad6e7242665adaf88de77b81b0b
fps <mista.tapas@gmx.net>
2022-11-04 21:59:16 +0100

    add module

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ config, lib, ...}:
+{
+  options.make-nixos-fast-again.enable = mkOption {
+    type lib.types.bool;
+    default = false;
+  }
+
+  config.boot.kernelParams = mkIf config.make-nixos-fast-again.enable [
+    "noibrs" "noibpb" "nopti" "nospectre_v2" "nospectre_v1" "l1tf=off"
+    "nospec_store_bypass_disable" "no_stf_barrier" "mds=off" "tsx=on"
+    "tsx_async_abort=off" "mitigations=off" "processor.max_cstate=1"
+    "intel_idle.max_cstate=0"
+  ];
+}
