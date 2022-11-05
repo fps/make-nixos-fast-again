aa2d7fcd797de990e228a1b2512411bf6943cec7
fps <mista.tapas@gmx.net>
2022-11-05 12:56:38 +0100

    make it simpler - disabling can be done by commenting out the import

--- default.nix
+++ default.nix
@@ -1,11 +1,6 @@
 { config, lib, ...}:
 {
-  options.make-nixos-fast-again.enable = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
-  };
-
-  config.boot.kernelParams = lib.mkIf config.make-nixos-fast-again.enable [
+  config.boot.kernelParams = [
     "noibrs" "noibpb" "nopti" "nospectre_v2" "nospectre_v1" "l1tf=off"
     "nospec_store_bypass_disable" "no_stf_barrier" "mds=off" "tsx=on"
     "tsx_async_abort=off" "mitigations=off"
