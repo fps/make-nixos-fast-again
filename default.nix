7f0b263463aea81d527c546a8efb8ed197b33d94
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-11-05 03:39:33 +0100

    fix copy and paste error

--- default.nix
+++ default.nix
@@ -8,7 +8,6 @@
   config.boot.kernelParams = lib.mkIf config.make-nixos-fast-again.enable [
     "noibrs" "noibpb" "nopti" "nospectre_v2" "nospectre_v1" "l1tf=off"
     "nospec_store_bypass_disable" "no_stf_barrier" "mds=off" "tsx=on"
-    "tsx_async_abort=off" "mitigations=off" "processor.max_cstate=1"
-    "intel_idle.max_cstate=0"
+    "tsx_async_abort=off" "mitigations=off"
   ];
 }
